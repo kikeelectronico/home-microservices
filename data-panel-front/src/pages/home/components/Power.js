7e36ebd5a69cbff68adb0af31d07c45637579dce
Enrique <enrique.gomez.electronico@gmail.com>
2025-07-06 00:10:18 +0200

    fix tab

--- data-panel-front/src/pages/home/components/Power.js
+++ data-panel-front/src/pages/home/components/Power.js
@@ -7,25 +7,25 @@ export default function Power(props) {
         return (props.home.status.current001.brightness * 35)
     }
 
-  return (
-    <div className="homeCard" style={{boxShadow: "0 0.1rem 1rem rgba(" + (props.home.status.current001.brightness > 90 ? "255,0,0" : "0,0,0")  + ", 0.8)"}}>
-        <div className="homeCardTitle">
-            Potencia
-        </div>
-        <div className="homeCardRow">
-            <div className="powerCardPowerContainer">
-                {calcPower()} W
+    return (
+        <div className="homeCard" style={{boxShadow: "0 0.1rem 1rem rgba(" + (props.home.status.current001.brightness > 90 ? "255,0,0" : "0,0,0")  + ", 0.8)"}}>
+            <div className="homeCardTitle">
+                Potencia
             </div>
-        </div>
-        {
-            props.home.status.current001.brightness > 90 ?
-                <div className="homeCardRow">
-                    <div className="powerCardAlertContainer">
-                        Sobrecarga de potencia
-                    </div>
+            <div className="homeCardRow">
+                <div className="powerCardPowerContainer">
+                    {calcPower()} W
                 </div>
-            : <></>
-        }
-    </div>
-  )
+            </div>
+            {
+                props.home.status.current001.brightness > 90 ?
+                    <div className="homeCardRow">
+                        <div className="powerCardAlertContainer">
+                            Sobrecarga de potencia
+                        </div>
+                    </div>
+                : <></>
+            }
+        </div>
+    )
 }
