dddc526ffa7b6bc7a8fbe6966f9560dc3405370b
kikeelectronico <enrique.gomez.electronico@gmail.com>
2025-09-05 21:07:02 +0200

    turn work table on only if sofa unoccupied

--- logic-pool/sensors.py
+++ logic-pool/sensors.py
@@ -29,6 +29,6 @@ def bedroom(homeware, topic, payload):
 
 def workTable(homeware, topic, payload):
   if topic == "device/0b97c3c8-cb02-4f6d-9e60-d5755b25b968_1/occupancy":
-    if payload == "OCCUPIED":
+    if payload == "OCCUPIED" and homeware.get("pressure001", "occupancy" == "UNOCCUPIED"):
       homeware.execute("hue_9", "on", True)
       homeware.execute("hue_10", "on", True)
