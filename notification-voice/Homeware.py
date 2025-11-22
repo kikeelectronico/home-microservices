47a749ba31a2c6db02142e1f92dfa9edf6a505e8
kikeelectronico <enrique.gomez.electronico@gmail.com>
2025-11-22 19:13:07 +0100

    remove debbuging

--- notification-voice/Homeware.py
+++ notification-voice/Homeware.py
@@ -32,18 +32,18 @@ class Homeware:
       self._fail_to_update = True
       logging.error("Homeware env vars aren't set")
     else:
-      # try:
-      url = self.__url + "/api/devices/" + id + "/states/" + param
-      headers = {"Authorization": "bearer " + self.__token}
-      response = requests.get(url, headers=headers)
-      if response.status_code == 200:
-        return response.json()
-      else:
-        logging.warning("Fail to get Homeware status. Status code: " + str(response.status_code))
-        return (False, {})
-      # except (requests.ConnectionError, requests.Timeout) as exception:
-      #   logging.warning("Fail to get Homeware status. Conection error.")
-      #   self._fail_to_update = False
+      try:
+        url = self.__url + "/api/devices/" + id + "/states/" + param
+        headers = {"Authorization": "bearer " + self.__token}
+        response = requests.get(url, headers=headers)
+        if response.status_code == 200:
+          return response.json()
+        else:
+          logging.warning("Fail to get Homeware status. Status code: " + str(response.status_code))
+          return (False, {})
+      except (requests.ConnectionError, requests.Timeout) as exception:
+        logging.warning("Fail to get Homeware status. Conection error.")
+        self._fail_to_update = False
 
   def getDevices(self):
     if self.__token == "no_set" or self.__url == "no_set":
