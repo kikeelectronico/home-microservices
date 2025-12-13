045b6ecffbdf95117d50e2db71e02a6655e15db2
kikeelectronico <enrique.gomez.electronico@gmail.com>
2025-12-13 19:06:35 +0100

    round and calc once

--- device-controller/lights.py
+++ device-controller/lights.py
@@ -55,8 +55,9 @@ def worktableLight(homeware, topic, payload):
         homeware.execute("hue_9", "brightness", 10)
         homeware.execute("hue_10", "brightness", 10)
       else:
-        homeware.execute("hue_9", "brightness", light_level*WORKTABLE_DYNAMIC_BRIGNTNESS_MULTIPLIER)
-        homeware.execute("hue_10", "brightness", light_level*WORKTABLE_DYNAMIC_BRIGNTNESS_MULTIPLIER)
+        brightness = int(light_level*WORKTABLE_DYNAMIC_BRIGNTNESS_MULTIPLIER)
+        homeware.execute("hue_9", "brightness", brightness)
+        homeware.execute("hue_10", "brightness", brightness)
 
   if topic == "device/0b97c3c8-cb02-4f6d-9e60-d5755b25b968_1/occupancy":
     if payload == "OCCUPIED" and homeware.get("pressure001", "occupancy") == "UNOCCUPIED":
