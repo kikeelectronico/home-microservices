a805ece7c0b3e9a895f23851062e718427952235
kikeelectronico <hola@enriquegomez.me>
2026-04-05 21:04:47 +0200

    move validation to constructor

--- hue-outbound/hue.py
+++ hue-outbound/hue.py
@@ -13,28 +13,34 @@ class Hue:
   def __init__(self, url, token):
     self.__url = url
     self.__token = token
+
+    fail = False
+    if self.__url == "no_set":
+      logging.error("HUE_HOST env var isn't set")
+      fail = True
+    if self.__token == "no_set":
+      logging.error("HUE_TOKEN env var isn't set")
+      fail = True
+    if fail:
+      exit()
       
   # Get motion
   def getResource(self, resource="device"):
-    if self.__token == "no_set" or self.__url == "no_set":
-      self._fail_to_update = True
-      logging.error("Hue env vars aren't set")
-    else:
-      try:
-        url = "https://" + self.__url + "/clip/v2/resource/" + resource
-        headers = {
-          'hue-application-key': self.__token
-        }
-        response = requests.get(url, headers=headers, verify=False)
-        if response.status_code == 200:
-          return response.json()["data"]
-        else:
-          logging.warning("Fail to get Hue Bridge " + resource + ". Status code: " + str(response.status_code))
-          return {}
-      except (requests.ConnectionError, requests.Timeout) as exception:
-          logging.warning("Fail to get Hue Bridge " + resource + ". Conection error.")
-          self._fail_to_update = False
-          return {}
+    try:
+      url = "https://" + self.__url + "/clip/v2/resource/" + resource
+      headers = {
+        'hue-application-key': self.__token
+      }
+      response = requests.get(url, headers=headers, verify=False)
+      if response.status_code == 200:
+        return response.json()["data"]
+      else:
+        logging.warning("Fail to get Hue Bridge " + resource + ". Status code: " + str(response.status_code))
+        return {}
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        logging.warning("Fail to get Hue Bridge " + resource + ". Conection error.")
+        self._fail_to_update = False
+        return {}
       
   def sendToHue(self, hue_id, hue_status):
     try:
