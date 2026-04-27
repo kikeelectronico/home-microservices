4e955356dfc84ee98cd78e71754395d177a359a6
kikeelectronico <hola@enriquegomez.me>
2026-04-27 21:19:21 +0200

    add bedroom light control

--- decision-engine/engine/registry.py
+++ decision-engine/engine/registry.py
@@ -1,5 +1,6 @@
 from typing import List
 from domains.lighting.button_lighting import ButtonLightingHandler
+from domains.lighting.bedroom import BedroomLightHandler
 from domains.lighting.hallway import HallwayLightHandler
 from domains.scenes.sensors import SensorsSceneHandler
 from engine.engine import Handler
@@ -8,6 +9,7 @@ from engine.engine import Handler
 def build_handlers() -> List[Handler]:
     return [
         ButtonLightingHandler(),
+        BedroomLightHandler(),
         HallwayLightHandler(),
         SensorsSceneHandler(),
     ]
--- decision-engine/infrastructure/inbound/mqtt_config.py
+++ decision-engine/infrastructure/inbound/mqtt_config.py
@@ -1,4 +1,5 @@
 TOPICS = [
     "device/interruptor_prueba/on",
     "device/ee2fcd12-9b2e-478f-826f-a4a5447d3a27/occupancy",
+    "device/c2b38173-883e-4766-bcb5-0cce2dc0e00e/occupancy",
 ]
