700370d488b5676009a0cbce46861302fa2b6714
kikeelectronico <hola@enriquegomez.me>
2026-05-19 01:41:24 +0200

    update logging format

--- hue-outbound/main.py
+++ hue-outbound/main.py
@@ -106,7 +106,7 @@ def on_message(client, userdata, msg):
 if __name__ == "__main__":
 	logging.basicConfig(
 		level=logging.INFO,
-		format="%(asctime)s %(levelname)-10s %(name)-5s %(message)s"
+		format="%(asctime)s %(levelname)-8s %(name)-12s - %(message)s"
 	)
 	# Check env vars
 	def report(message):
