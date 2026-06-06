a3bc542b3cb0f2f322819759b54d4fe3de7a7d37
kikeelectronico <hola@enriquegomez.me>
2026-06-06 19:28:43 +0200

    verify envs only once

--- ikea-outbound/ikea.py
+++ ikea-outbound/ikea.py
@@ -9,46 +9,50 @@ class Ikea:
   def __init__(self, host, token):
     self.__host = host
     self.__token = token
+
+    fail = False
+    if self.__host == "no_set":
+      logging.error("IKEA_HOST env var isn't set")
+      fail = True
+    if self.__token == "no_set":
+      logging.error("IKEA_TOKEN env var isn't set")
+      fail = True
+    if fail:
+      exit()
       
   # Get device
   def getDevices(self, device_id="all"):
-    if self.__token == "no_set" or self.__host == "no_set":
-      logging.error("IKEA env vars aren't set")
-    else:
-      try:
-        session = requests.Session()
-        session.headers.update({
-            "Authorization": f"Bearer {self.__token}"
-        })
-        url = f"https://{self.__host}:8443/v1/devices"
-        if device_id != "all":
-          url = f"{url}/{device_id}"
-        response = session.get(url, verify=False)
-        response.raise_for_status()
-        devices = response.json()
-        return devices
-      except (requests.ConnectionError, requests.Timeout) as exception:
-          logging.warning("Fail to get the IKEA device %s. Connection error.", device_id)
-          if device_id == "all": return []
-          return {}
+    try:
+      session = requests.Session()
+      session.headers.update({
+          "Authorization": f"Bearer {self.__token}"
+      })
+      url = f"https://{self.__host}:8443/v1/devices"
+      if device_id != "all":
+        url = f"{url}/{device_id}"
+      response = session.get(url, verify=False)
+      response.raise_for_status()
+      devices = response.json()
+      return devices
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        logging.warning("Fail to get the IKEA device %s. Connection error.", device_id)
+        if device_id == "all": return []
+        return {}
       
   # Update device
   def setDevice(self, device_id="all", attribute="isOn", value=False):
-    if self.__token == "no_set" or self.__host == "no_set":
-      logging.error("IKEA env vars aren't set")
-    else:
-      try:
-        session = requests.Session()
-        session.headers.update({
-            "Authorization": f"Bearer {self.__token}"
-        })
-        url = f"https://{self.__host}:8443/v1/devices/{device_id}"
-        payload = [{"attributes": {}}]
-        payload[0]["attributes"][attribute] = value
-        response = session.patch(url, json=payload, verify=False)
-        response.raise_for_status()
-        return response.status_code == 202
-      except (requests.ConnectionError, requests.Timeout) as exception:
-          logging.warning("Fail to update the IKEA device %s. Connection error.", device_id)
-          return False
+    try:
+      session = requests.Session()
+      session.headers.update({
+          "Authorization": f"Bearer {self.__token}"
+      })
+      url = f"https://{self.__host}:8443/v1/devices/{device_id}"
+      payload = [{"attributes": {}}]
+      payload[0]["attributes"][attribute] = value
+      response = session.patch(url, json=payload, verify=False)
+      response.raise_for_status()
+      return response.status_code == 202
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        logging.warning("Fail to update the IKEA device %s. Connection error.", device_id)
+        return False
       
