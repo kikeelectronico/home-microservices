3cac8fee528f5053dbe015a343903b0055957b3f
kikeelectronico <hola@enriquegomez.me>
2026-06-06 19:30:02 +0200

    migrate to requests.method

--- ikea-outbound/ikea.py
+++ ikea-outbound/ikea.py
@@ -23,14 +23,13 @@ class Ikea:
   # Get device
   def getDevices(self, device_id="all"):
     try:
-      session = requests.Session()
-      session.headers.update({
-          "Authorization": f"Bearer {self.__token}"
-      })
+      headers = {
+        "Authorization": f"Bearer {self.__token}"
+      }
       url = f"https://{self.__host}:8443/v1/devices"
       if device_id != "all":
         url = f"{url}/{device_id}"
-      response = session.get(url, verify=False)
+      response = requests.get(url, headers=headers, verify=False)
       response.raise_for_status()
       devices = response.json()
       return devices
@@ -42,14 +41,13 @@ class Ikea:
   # Update device
   def setDevice(self, device_id="all", attribute="isOn", value=False):
     try:
-      session = requests.Session()
-      session.headers.update({
-          "Authorization": f"Bearer {self.__token}"
-      })
+      headers = {
+        "Authorization": f"Bearer {self.__token}"
+      }
       url = f"https://{self.__host}:8443/v1/devices/{device_id}"
       payload = [{"attributes": {}}]
       payload[0]["attributes"][attribute] = value
-      response = session.patch(url, json=payload, verify=False)
+      response = requests.patch(url, json=payload, headers=headers, verify=False)
       response.raise_for_status()
       return response.status_code == 202
     except (requests.ConnectionError, requests.Timeout) as exception:
