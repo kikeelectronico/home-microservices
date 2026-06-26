a93c73dbf5706b2ca515f01fb1a23253871dbeb6
kikeelectronico <hola@enriquegomez.me>
2026-06-26 21:35:14 +0200

    standardize code

--- hue-sensors-alerts/hue.py
+++ hue-sensors-alerts/hue.py
@@ -3,6 +3,8 @@ import logging
 from sseclient import SSEClient
 import time
 
+REQUEST_TIMEOUT = 10
+
 class Hue:
   
   __url = "localhost"
@@ -11,28 +13,33 @@ class Hue:
   def __init__(self, url, token):
     self.__url = url
     self.__token = token
+
+    fail = False
+    if self.__url == "no_set":
+      logging.error("HUE_HOST env var isn't set")
+      fail = True
+    if self.__token == "no_set":
+      logging.error("HUE_TOKEN env var isn't set")
+      fail = True
+    if fail:
+      exit()
       
-  # Get motion
-  def getResource(self, resource="device"):
-    if self.__token == "no_set" or self.__url == "no_set":
-      self._fail_to_update = True
-      logging.error("Hue env vars aren't set")
-    else:
-      try:
-        url = "https://" + self.__url + "/clip/v2/resource/" + resource
-        headers = {
-          'hue-application-key': self.__token
-        }
-        response = requests.get(url, headers=headers, verify=False)
-        if response.status_code == 200:
-          return response.json()["data"]
-        else:
-          logging.warning("Fail to get Hue Bridge " + resource + ". Status code: " + str(response.status_code))
-          return {}
-      except (requests.ConnectionError, requests.Timeout) as exception:
-          logging.warning("Fail to get Hue Bridge " + resource + ". Conection error.")
-          self._fail_to_update = False
-          return {}
+  # Get services
+  def getServices(self, type="device"):
+    try:
+      url = "https://" + self.__url + "/clip/v2/resource/" + type
+      headers = {
+        'hue-application-key': self.__token
+      }
+      response = requests.get(url, headers=headers, verify=False, timeout=REQUEST_TIMEOUT)
+      if response.status_code == 200:
+        return response.json()["data"]
+      else:
+        logging.warning("Fail to get the " + type + " services from Hue Bridge. Status code: " + str(response.status_code))
+        return []
+    except (requests.ConnectionError, requests.Timeout) as exception:
+        logging.warning("Fail to get the " + type + " services from Hue Bridge. Connection error.")
+        return []
 
   def getEventStreamClient(self):
     while True:
