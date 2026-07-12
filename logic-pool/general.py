1bc05be2bcb69eb45d2d680d7af6f787524a6488
kikeelectronico <hola@enriquegomez.me>
2026-07-12 21:10:13 +0200

    migrate livingroom thermostat logic related with leaving home to the decision engine

--- logic-pool/general.py
+++ logic-pool/general.py
@@ -2,10 +2,6 @@
 def atHome(homeware, topic, payload):
   if topic == "device/switch_at_home/on":
     if payload:
-      if homeware.get("scene_winter", "enable"):
-        homeware.execute("thermostat_livingroom", "thermostatTemperatureSetpoint", 22)
-        homeware.execute("thermostat_livingroom", "thermostatMode", "heat")
       homeware.execute("switch_prepare_home", "on", False)
     else:
-      homeware.execute("thermostat_livingroom", "thermostatMode", "off")
       homeware.execute("thermostat_livingroom", "thermostat_bathroom", "off")
