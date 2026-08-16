5218a7d42b49ae13a5856a9d02faa2c90684a92e
kikeelectronico <hola@enriquegomez.me>
2026-08-17 00:07:01 +0200

    simplify main file

--- meteo-inflow/main.py
+++ meteo-inflow/main.py
@@ -1,13 +1,11 @@
 import paho.mqtt.client as mqtt
-import xml.etree.ElementTree as ElementTree
-from datetime import date, datetime
 import os
 import time
-import requests
 import logging
 import json
 
 from weather import getWeather
+from warnings import getWarnings
 
 # Load env vars
 if os.environ.get("MQTT_PASS", "no_set") == "no_set":
@@ -41,75 +39,16 @@ mqtt_client = mqtt.Client(
   protocol=mqtt.MQTTv5
 )
 
-def relative_day(text: str) -> int:
-    try:
-        target_date = datetime.fromisoformat(text).date()
-    except ValueError:
-        return -1
-
-    return (target_date - date.today()).days
-
-def is_alert_active(start_text: str, end_text: str) -> bool:
-    try:
-        start_dt = datetime.fromisoformat(start_text)
-        end_dt = datetime.fromisoformat(end_text)
-    except ValueError:
-        return False
-
-    now = datetime.now(start_dt.tzinfo)
-
-    return start_dt <= now <= end_dt
-
 def publishWarnings(force=False):
-    global last_build_date
-    # Get AEMET RSS feed
-    feed_data = None
-    url = AEMET_RSS
-    response = requests.get(url, timeout=REQUEST_TIMEOUT)
-    if response.status_code == 200:
-        feed_data = response.text
-    else:
-      logging.warning("Fail to get AEMET RSS feed. Status code: " + str(response.status_code))
+  global last_build_date
+  warnings_payload = getWarnings(AEMET_RSS, AEMET_AREA, REQUEST_TIMEOUT)
+  if not warnings_payload:
+    return
 
-    # Manipulate data
-    if feed_data:
-      feed_root = ElementTree.fromstring(feed_data)
-      build_date = feed_root.find("channel").find("lastBuildDate").text
-      if force or build_date != last_build_date:
-        warnings = []
-        for item in feed_root.find("channel").findall("item"):
-          title = item.find("title").text
-          if AEMET_AREA in title:
-            link = item.find("link").text
-            # Gey warnings
-            warning_data = None
-            response = requests.get(link, timeout=REQUEST_TIMEOUT)
-            if response.status_code == 200:
-                warning_data = response.text
-            else:
-              logging.warning("Fail to get AEMET warning. Status code: " + str(response.status_code))
-            # Create and publish warning
-            if warning_data:
-              warning = {}
-              ns = {"cap": "urn:oasis:names:tc:emergency:cap:1.2"}
-              warning_root = ElementTree.fromstring(warning_data)
-              for info in warning_root.findall("cap:info", ns):
-                 if info.find("cap:language", ns).text == "es-ES":
-                  warning["title"] = info.find("cap:headline", ns).text.split(AEMET_AREA)[0]
-                  warning["description"] = info.find("cap:description", ns).text
-                  warning["starts"] = info.find("cap:onset", ns).text
-                  warning["ends"] = info.find("cap:expires", ns).text
-                  warning["start_offset"] = relative_day(warning["starts"])
-                  warning["is_active"] = is_alert_active(warning["starts"], warning["ends"])
-                  for parameter in info.findall("cap:parameter", ns):
-                    if parameter.find("cap:valueName", ns).text == "AEMET-Meteoalerta nivel":
-                      warning["level"] = parameter.find("cap:value", ns).text
-                    if parameter.find("cap:valueName", ns).text == "AEMET-Meteoalerta probabilidad":
-                      warning["probability"] = parameter.find("cap:value", ns).text
-
-              warnings.append(warning)
-        mqtt_client.publish("meteo/warnings", json.dumps(warnings))
-        last_build_date = build_date
+  warnings, build_date = warnings_payload
+  if force or build_date != last_build_date:
+    mqtt_client.publish("meteo/warnings", json.dumps(warnings))
+    last_build_date = build_date
 
 def publishWeather(force=False):
     global last_weather_payload
