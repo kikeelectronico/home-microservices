297f4120e2294bef912b0ac839b070c8f414ebaa
kikeelectronico <hola@enriquegomez.me>
2026-08-17 00:50:31 +0200

    fix water readings

--- water-inflow/water.py
+++ water-inflow/water.py
@@ -19,13 +19,12 @@ def getWater(url=WATER_URL, timeout=10):
       return None
 
     tree = html.fromstring(page.content)
-    last_update = _first(tree.xpath('//*[@id="index_bodycenter"]/div[2]/div[2]/div[3]/div[1]/strong/text()'))
-    level = _first(tree.xpath('//*[@id="index_bodycenter"]/div[2]/div[2]/div[3]/div[4]/strong/text()'))
+    last_update = str(_first(tree.xpath('//*[@id="index_bodycenter"]/div[2]/div[2]/div[3]/div[1]/strong/text()')))
+    level = str(_first(tree.xpath('//*[@id="index_bodycenter"]/div[2]/div[2]/div[3]/div[4]/strong/text()'))).replace(",",".")
 
     if not last_update or not level:
       logging.warning("Fail to parse embalses.net response.")
       return None
-
     return {
       "level": float(level),
       "last_update": last_update.split("(")[1].split(")")[0],
