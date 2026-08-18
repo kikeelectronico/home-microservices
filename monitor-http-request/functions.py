35af8ce418331d720c86ed7fefd20eeaf2d342b6
kikeelectronico <hola@enriquegomez.me>
2026-08-19 00:52:26 +0200

    add ikea bridge to http monitor

--- monitor-http-request/functions.py
+++ monitor-http-request/functions.py
@@ -1,6 +1,9 @@
 import requests
 import logging
 
+import urllib3
+urllib3.disable_warnings()
+
 REQUEST_TIMEOUT = 10
 
 # Test both the API and the db getting the status of a device
@@ -36,3 +39,19 @@ def hueTest(api_url, api_token):
   except requests.ConnectionError:
     logging.warning("Unable to connect to Hue Bridge")
     return False
+
+# Test Ikea Bridge
+def ikeaTest(api_url, api_token):     
+  try:
+    url = f"https://{api_url}:8443/v1/devices"
+    headers = {
+      "Authorization": f"Bearer {api_token}"
+    }
+    response = requests.get(url, headers=headers, verify=False, timeout=REQUEST_TIMEOUT)
+    if response.status_code == 200:
+      return True
+    logging.warning("Fail to get devices from Ikea Bridge. Status code: " + str(response.status_code))
+    return False
+  except (requests.ConnectionError, requests.Timeout) as exception:
+      logging.warning("Fail to get devices from Ikea Bridge. Connection error.")
+      return False
