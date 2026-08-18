1d60022ccf2f7f2d2e2cf3907b05fd984a66bb02
kikeelectronico <hola@enriquegomez.me>
2026-08-19 00:23:54 +0200

    return internet_status object

--- internet-inflow/Internet.py
+++ internet-inflow/Internet.py
@@ -4,7 +4,11 @@ import requests
 def getInternetStatus():
   try:
     requests.get("https://www.google.com", timeout=2)
-    return True
+    return {
+      "connected": True
+    }
   except (requests.ConnectionError, requests.Timeout) as exception:
     logging.warning("Fail to reach Google. Conection error.")
-    return False
+    return {
+      "connected": False
+    }
